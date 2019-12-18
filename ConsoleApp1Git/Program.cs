cda8be2fb22cbb18ea81f9676372a4b0774fc3ca
ManishKumarThakur <manishkr_008@yahoo.com>
2019-12-18 23:46:18 +0530

    Update Program.cs

--- ConsoleApp1Git/Program.cs
+++ ConsoleApp1Git/Program.cs
@@ -10,9 +10,7 @@ namespace ConsoleApp1Git
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Deggggggbug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
+            //dansole.WriteLine("Hello World!");
             Console.ReadKey();
             Console.WriteLine("123..5590");
             // fsdfsd
