f7d26269b3cbd3ecd8b26caf0254918c8f9a639f
ManishKumarThakur <manishkr_008@yahoo.com>
2019-12-19 10:30:15 +0530

    Take the plan

--- ConsoleApp1Git/Feature2.cs
+++ ConsoleApp1Git/Feature2.cs
@@ -8,5 +8,14 @@ namespace ConsoleApp1Git
 {
     class Feature2
     {
+        public int AddNumbers()
+        {
+            int x = 9;
+            int y = 10;
+
+            int sum = x + y;
+
+            return sum;
+        }
     }
 }
