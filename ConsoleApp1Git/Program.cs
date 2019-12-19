20949c89905a1efc058b617e5f2f1e5c6bb8671b
ManishKumarThakur <manishkr_008@yahoo.com>
2019-12-19 07:20:54 +0530

    Update Program.cs

--- ConsoleApp1Git/Program.cs
+++ ConsoleApp1Git/Program.cs
@@ -12,7 +12,7 @@ namespace ConsoleApp1Git
         {
             //dansole.WriteLine("Hello World!");
             Console.ReadKey();
-            Console.WriteLine("123..559011");
+            Console.WriteLine("123..669011");
             // fsdfsd
             
             //santu
